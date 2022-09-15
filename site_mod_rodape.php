1381cdaf896808bdd68378538f87815849790685
Edson Araújo <edson.araujo@basis.com.br>
2022-09-14 22:06:44 -0400

    link menu

--- site_mod_rodape.php
+++ site_mod_rodape.php
@@ -4,9 +4,6 @@
 <ul>
     <li ><a href="index.php" >Home  </a> </li>
     <li ><a href="efas.php" >O que é o EFAS?  </a> </li>
-    <li><a href="cursos.php"  >Cursos </a></li>
     <li><a href="inscricao.php" >Inscrição </a></li>
-    <li><a href="casas-espiritas.php" >Casas Espíritas </a></li>
-    <li><a href="fotos.php">Fotos </a></li>
     <div class="clearfix"> </div>
 </ul>
--- site_mod_topo.php
+++ site_mod_topo.php
@@ -6,18 +6,15 @@
     <ul>
         <li ><a href="index.php" class="hvr-sweep-to-bottom color"><i class="glyphicon glyphicon-home"></i>Home  </a> </li>
         <li ><a href="efas.php" class="hvr-sweep-to-bottom color1"><i class="glyphicon glyphicon-picture"></i>O que é o EFAS?  </a> </li>
-        <li><a href="cursos.php"  class="hvr-sweep-to-bottom color2"><i class="glyphicon glyphicon-tags"></i>Cursos</a></li>
 		<li><a href="inscricao.php" class="hvr-sweep-to-bottom color1"><i class="glyphicon glyphicon-calendar"></i>Inscrição </a></li>
-		<li><a href="casas-espiritas.php" class="hvr-sweep-to-bottom color3"><i class="glyphicon glyphicon-search"></i>Casas Espíritas </a></li>
-		<li><a href="fotos.php" class="hvr-sweep-to-bottom color4"><i class="glyphicon glyphicon-envelope"></i>Fotos </a></li>
-    <div class="clearfix"> </div>
+        <div class="clearfix"> </div>
     </ul>
     <!--script-->
-<script>
-    $("span.menu").click(function(){
-        $(".top-nav ul").slideToggle(500, function(){
+    <script>
+        $("span.menu").click(function(){
+            $(".top-nav ul").slideToggle(500, function(){
+            });
         });
-    });
-</script>				
+    </script>				
 </div>
 <div class="clearfix"> </div>
