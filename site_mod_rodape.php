5ba274efd70e60b3856336eee6487d424d33137a
Edson Galdino <edsongaldino@outlook.com>
2024-08-05 21:35:47 -0400

    Ajuste Rodapé

--- site_mod_rodape.php
+++ site_mod_rodape.php
@@ -4,7 +4,7 @@
 <ul>
     <li ><a href="index.php" >Home  </a> </li>
     <li ><a href="efas.php" >O que é o EFAS?  </a> </li>
-    <!--<li><a href="inscricao.php" >Inscrição </a></li>-->
+    <li><a href="inscricao.php" >Inscrição </a></li>
     <div class="clearfix"> </div>
 </ul>
 
